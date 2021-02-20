13920eca49e4cf7e8e6f0c38493796c8bc587cdf
vricci518 <78501210+vricci518@users.noreply.github.com>
2021-02-20 12:30:56 -0500

    Homework

---
+++ homework/popquiz.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+        <body>
+<?php>
+$x = 0;
+if($x){
+    echo "The value of x is $x<br/>";
+} else {
+    echo 'The value of x is $x'<br/>;
+}
+?>
+        </body>
+</html>
