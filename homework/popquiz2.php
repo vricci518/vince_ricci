862076c3f9e053f028b6cd2b636c417cbd1b327e
vricci518 <78501210+vricci518@users.noreply.github.com>
2021-03-04 11:31:31 -0500

    updated file

---
+++ homework/popquiz2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Test</title>
+</head>
+
+<body>
+   
+<?php
+$array = array();
+
+if(!empty($array)){
+    echo 'The array has '.length($array).' elements';
+}
+echo 'End processing';
+?>
+</body>
+
+</html>
