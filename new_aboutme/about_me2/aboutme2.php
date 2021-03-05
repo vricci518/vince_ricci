cfcb1e126360114e7e0e98c6381a214de25e3f0d
vricci518 <78501210+vricci518@users.noreply.github.com>
2021-03-05 15:18:54 -0500

    revised files

--- new_aboutme/about_me2/aboutme2.php
+++ new_aboutme/about_me2/aboutme2.php
@@ -16,6 +16,7 @@
 
 <body>
 	<?php include 'header.php'; ?>
+	<!-- Carousel -->
 	<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
 		<div class="carousel-inner">
 			<div class="carousel-item active">
@@ -29,18 +30,31 @@
 			</div>
 		</div>
 	</div>
+	<!-- Intro page information -->
+<div class="container-fluid" id="intro">
+	<h2>A Little Bit About Me</h2>
+	<div>
+		<p>
+			Hi there, my name is Vince and welcome to my page. So let's start with some information about me. I'm from Buffalo, New York and I've lived here all of my life. I went to college at SUNY Buffalo State College where I received a Bachelors of Fine Arts degree. I am a Graphic Designer/Graphic Artist who has worked in a variety of positions in the field: involving printing, pre-press, design and layout, marketing, e-commerce, and social media. I'm currently studying Web development to expand my opportunities to include Web Design and Front End Development.</p>
+		<p>
+			Outside of my professional career I have a variety of interests that I'm involved with. I have a long-time interest in charity fund-raising and have been a member of <em>The Imperial Court of Buffalo</em> since 1994. The Imperial Court is a grassroots organization in Western New York that is affliated with the <em>International Court System</em>. It is a 501-(c)3 Not For Profit that raises funds for various AIDS/HIV and LGBTQ+ charities in Buffalo, NY and the surrounding Western New York Area. During my time as a member I've held numerous postions within the Board of Directors as well as two terms as <em>"Emperor,"</em> which is one of the two elected fundraising chairs of the organization. I currently sit as the President of the Board of Directors.
+		</p>
+		<p>
+			I am also an avid knitter, having first learning how to knit in the late 1980's. I am a long time member of <em>The Knitting Guild of Greater Buffalo</em> and one of the few male knitters within the group. But our numbers are growing. I have also become a member of <em>The Men Who Knit</em> online community, as well as having attended several of the regional <em>Men's Knitting Retreats</em>. 
+		</p>
+		<p>
+			In Ocotber of 2020 I entered the world of Podcasting by creating a YouTube Video Channel called <em>Boozie Bears Knitting Podcast</em>. Together with my good friend Justin, we co-host an online video program about knitting and the fiber arts. We publish a new episode every 2-3 weeks and each episode runs between 60 and 90 minutes. In each episode we discuss and show what we are currently working on, what we have completed, purchases and fiber related acquisitions, as well as anything else we feel the need to discuss. We have a growing following of subscribers and recently held our very first LIVE broadcast. 
+		</p>
+		<p>
+			Be sure to check out the other pages of my site as well as my Links for more information on all of this.
+		</p>
+	</div>
+
+</div>
+
 
 	<!-- Option 1: Bootstrap Bundle with Popper -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
-	<div class="container-fluid">
-		<h2>A Little Bit About Me</h2>
-		<div >
-			<p>
-				Hi there, my name is Vince and welcome to my page. So let's start with some information about me. I'm from Buffalo, New York and I've lived here all of my life. I went to college at SUNY Buffalo State College where I received a Bachelors of Fine Arts degree. I am a Graphic Designer/Graphic Artist who has worked in a variety of positions in the field, involving printing, pre-press, design and layout, marketing, e-commerce, and social media.
-			</p>
-		</div>
-
-	</div>
 
 </html>
