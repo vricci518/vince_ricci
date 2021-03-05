1faadb4916e7f40f1267df59da5d055482e372cc
vricci518 <78501210+vricci518@users.noreply.github.com>
2021-03-05 16:53:17 -0500

    added common footer

--- new_aboutme/about_me2/projects.php
+++ new_aboutme/about_me2/projects.php
@@ -17,6 +17,8 @@
 <body>
 	<?php include 'header.php'; ?>
 	<h2>Knitting Projects</h2>
+
+	<?php include 'footer.php'; ?>
 	<!-- Option 1: Bootstrap Bundle with Popper -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
