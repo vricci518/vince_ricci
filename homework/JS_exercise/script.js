5fb201c64d7df2d9d5be37258bf8f1849b71039b
vricci518 <78501210+vricci518@users.noreply.github.com>
2021-03-08 22:01:14 -0500

    JS exercise

---
+++ homework/JS_exercise/script.js
@@ -0,0 +1,35 @@
+// var count = 1;
+
+// while(count <= 100){
+//     console.log("count is " + count);
+//     count = count + 2;
+// }
+
+// for (var count = 1; count <= 5; count++){
+//     console.log(count);
+// }
+
+// for(var i = 0; i < 32; i+=8){
+//     console.log(i);
+// }
+
+// for (var count = 1; count <= 21; count++) {
+// 	if (count % 2 !== 0) {
+// 		console.log(count);
+// 	}
+// }
+
+/* Coersion - shorter code that does the same as the above */
+// for (var count = 1; count <= 21; count++) {
+// 	if (count % 2) {
+// 		console.log(count);
+// 	}
+// }
+
+
+/* For Loop to spell out banana */
+var string = "banana";
+
+for(var i = 0; i < string.length; i++){
+	console.log(string[i]);
+}
