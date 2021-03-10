70baa0ebe501f2e8f8f67258513a6c3941208180
vricci518 <78501210+vricci518@users.noreply.github.com>
2021-03-09 23:15:20 -0500

    revisions home podcasts

--- new_aboutme/about_me2/footer.php
+++ new_aboutme/about_me2/footer.php
@@ -19,13 +19,12 @@
 		<div class="col-sm-12 col-md-3 link">
 			<h5>Links</h5>
 			<p>
-				<a href="aboutme2.php">Home</a><br />
+				<a href="index.php">Home</a><br />
 				<a href="https://vincent-ricci-graphics.myportfolio.com/work" target="_blank">My Graphic Design Porfolio</a><br />
 				<a href="podcasts.php">Knitting Podcasts</a><br />
 				<a href="projects.php">Knitting Projects</a><br />
 				<a href="dyeing.php">Yarn Dyeing</a><br />
 			</p>
 		</div>
-
 	</div>
 </footer>
