f3806aba3ea7b18d0448ab09ffc53fa299bea073
vricci518 <78501210+vricci518@users.noreply.github.com>
2021-03-13 16:18:04 -0500

    additional homework

--- homework/VRicci_31121/vinceRicci.js
+++ homework/VRicci_31121/vinceRicci.js
@@ -1,37 +1,35 @@
 // This first example only works if the non-number is in quotes. Otherwise it errors out.
 
-function isHighest(a, b, c) {
-	var maxValue = 0;
+// function isHighest(a, b, c) {
+// 	var maxValue = 0;
 
-	if (a > b){
-		maxValue = a;
-	} else {
-		maxValue = b;
-	}
-	if (c > maxValue) {
-		maxValue = c;
-	}
-	if (maxValue >= 'maxValue') {
-		maxValue = null;
-	}
-	return maxValue
-}
+// 	if (a > b){
+// 		maxValue = a;
+// 	} else {
+// 		maxValue = b;
+// 	}
+// 	if (c > maxValue) {
+// 		maxValue = c;
+// 	}
+// 	if (maxValue >= 'maxValue') {
+// 		maxValue = null;
+// 	}
+// 	return maxValue
+// }
 
 // This one still returns the highest number even if one is not a number
 
-function isHighest(a, b, c) {
-	var maxValue = 0;
+// function isHighest(num1, num2, num3) {
+// 	return Math.max(num1, num2, num3)
+// }
 
-	if (a > b){
-		maxValue = a;
+
+function isHighest(num1, num2, num3) {
+	maxNum = Math.max(num1, num2, num3);
+	if (maxNum = NaN) {
+		return null;
 	} else {
-		maxValue = b;
+		return maxNum;
 	}
-	if (c > maxValue) {
-		maxValue = c;
 	}
-	if (isNaN(maxValue)) {
-		maxValue = null;
-	}
-	return maxValue
 }
