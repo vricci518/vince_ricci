dcd7996c0bc1b528ad8ed88e4ad9e86132086ba7
vricci518 <78501210+vricci518@users.noreply.github.com>
2021-03-27 22:30:22 -0400

    revised projects page

--- new_aboutme/about_me2/projects.php
+++ new_aboutme/about_me2/projects.php
@@ -41,12 +41,9 @@
 				</div>
 				<div class="col">
 					<div class="card shadow-sm">
-					<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-							<title>Bernie Mittens</title>
-							<rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Bernie Mittens</text>
-						</svg>
-
+					<img src="img/berniemittens.jpg" class="card-img-top rounded-circle" alt="bernie mittens">
 						<div class="card-body">
+						<h3 class="card-title">Bernie Mittens</h3>
 							<p class="card-text">This pattern is a knock off of the mittens that Senator Bernie Sanders wore to the 2021 Presidential Inaugeration. The fiber community was abuzz about them and several patterns came out soon afterwards. This one was my favorite and I think was the best written pattern.</p>
 							<div class="d-flex justify-content-between align-items-center">
 								<div class="btn-group">
